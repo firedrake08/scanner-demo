091e0b061ca877661af4a94ce05395511a4e5bbc
firedrake08 <dhritiman.tamuli.saikia@gmail.com>
2025-01-05 12:25:33 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'scan-app';
+  nav!: Navigator | any;
+  ngOnInit(): void {
+    this.nav = navigator;
+    this.nav.usb.getDevices().then((d:any) => {
+      console.log(d)
+    });
+  }
 }
